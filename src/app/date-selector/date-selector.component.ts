0ea68069645e269fa73257c395495d8fca783e05
Ruben <rubenplazaro@gmail.com>
2018-02-04 13:02:33 +0100

    Styles

--- src/app/date-selector/date-selector.component.ts
+++ src/app/date-selector/date-selector.component.ts
@@ -22,7 +22,7 @@ export const MY_FORMATS = {
 @Component({
   selector: 'app-date-selector',
   templateUrl: './date-selector.component.html',
-  styleUrls: ['./date-selector.component.css'],
+  styleUrls: ['./date-selector.component.scss'],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
